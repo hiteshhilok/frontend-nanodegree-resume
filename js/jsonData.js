f922f38dd781bb70b3da35a7433c948dc3e636d0
hiteshhilok <hiteshhilok@gmail.com>
2016-07-20 18:13:34 +0530

    file added

---
+++ js/jsonData.js
@@ -0,0 +1,59 @@
+var jdata= {
+    "bio": {
+        "name": "Kristen Marie",
+        "role": "Manager",
+		"contacts": {
+            "mobile": "+91-3232323232",
+            "email": "kristen12@gmail.com",
+            "github": "kristenGit",
+            "twitter": "kristentwitter",
+            "location": "Indore"
+          },
+        "welcomeMessage" : "WEL COME",
+        "startDate" : "22 fab 2011",
+        "skills" : "java,php,js",
+        "biopic" : "http://i.imgur.com/pWU1Xbl.png"
+    },
+    
+   "education":{
+      
+            "schools":{
+                 "name": "Queen's College",
+                 "location":"Indore",
+                 "degree":"master",
+                 "majors":"MCA",
+                 "dates":"11 december 2011",
+                 "url": ""
+                },
+            "onlineCourses": {
+                 "title": "Testing",
+                 "school": "Github",
+                 "dates": "3 march 2014",
+                 "url": "www.w3schools.com",
+              "display": ""
+            }
+      },
+    
+      "work" :{
+            "jobs":
+             {
+                 "employer": "test",
+                 "title":"pizza delivery" ,
+                 "location":"Bhopal",
+                 "workStart":"1 march 2014" ,
+                 "dates":"23 jan 2016",
+                 "description":"At times you will wish to be able to create a table from dynamic information passed directly to DataTables, rather than having it At times you will wish to be able to create a table from dynamic information passed directly to DataTables, rather than having it"
+             }
+          },
+    
+       "projects" : { 
+                  "projects":{
+                  "title":"Resume porject",
+                  "projectStart":"15 march 2011",
+                  "dates":"21 july 2014",
+                  "description":"At times you will wish to be able to create a table from dynamic information passed directly to DataTables, rather than having it read from the document. This is achieved using the data option in the initialisation object, passing in an array of data to be used (like all other DataTables handled data, this can be arrays or objects ",
+                  "images":""
+               }
+        }
+}
+
